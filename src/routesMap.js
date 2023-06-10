f3df0f0d022c3073fb8418c990540b807beb2ced
Đoàn Tiến Đạt <datdt@zent.vn>
2023-06-10 09:10:56 +0700

    refactor base core

--- src/routesMap.js
+++ src/routesMap.js
@@ -2,13 +2,11 @@ import {NOT_FOUND} from 'redux-first-router';
 import {
     ROUTE_AUTH,
     ROUTE_HOME,
-    ROUTE_EMPLOY,
     ROUTE_PROFILE
 } from './state/modules/routing';
 import {loadHomePage} from './state/modules/home/saga';
 import {loadAuthPage} from "./state/modules/auth/saga";
 import {loadProfilePage} from "./state/modules/profile/saga";
-import {loadEmployPage} from "./state/modules/employ/saga";
 
 const routeMap = {
     [NOT_FOUND]: {
@@ -19,11 +17,6 @@ const routeMap = {
         path: '/',
         component: 'Home',
         saga: loadHomePage
-    },
-    [ROUTE_EMPLOY]: {
-        path: '/employ',
-        component: 'Employ',
-        saga: loadEmployPage
     },
     [ROUTE_AUTH]: {
         path: '/login',
--- src/state/modules/routing/index.js
+++ src/state/modules/routing/index.js
@@ -1,7 +1,12 @@
 export const ROUTE_HOME = 'route/ROUTE_HOME';
 export const ROUTE_AUTH = 'route/ROUTE_AUTH';
 export const ROUTE_PROFILE = 'route/ROUTE_PROFILE';
-export const ROUTE_EMPLOY = 'route/ROUTE_EMPLOY';
+export const ROUTE_OWNER_FRAME = 'route/ROUTE_OWNER_FRAME';
+export const ROUTE_PATIENT_ORDER = 'route/ROUTE_PATIENT_ORDER';
+export const ROUTE_RESTOCK = 'route/ROUTE_RESTOCK';
+export const ROUTE_ORDER_HISTORY = 'route/ROUTE_ORDER_HISTORY';
+export const ROUTE_PAYMENT_METHODS = 'route/ROUTE_PAYMENT_METHODS';
+export const ROUTE_USER_MANAGEMENT = 'route/ROUTE_USER_MANAGEMENT';
 
 export const selectRouteType = state => state.location.type;
 export const selectRoutesMap = state => state.location.routesMap;
